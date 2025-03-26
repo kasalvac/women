2bf1042999e4fcdd474143550a9ae1c607e7c06c
kasalvac <kasal.emailer@gmail.com>
2025-03-26 16:51:52 +0100

    decoding of data utf-8 facebook sucks

--- data_prep.py
+++ data_prep.py
@@ -1,17 +1,34 @@
+import re
 import json
+from functools import partial
+
+# function to fix the mojibake from double encoding
+fix_mojibake_escapes = partial(
+    re.compile(rb'\\u00([\da-f]{2})').sub, #replace all \u00hh JSON sequences with the byte the last two hex digits represent,
+    lambda m: bytes.fromhex(m[1].decode()),
+)
+
+
 
 output_text = ''
 
-for i in range(1,21):
+for i in range(1,22):
+    print(i)
 
     dir_name = ('data/message_' + str(i) + '.json')
-    with open(dir_name, 'r', encoding='utf-8') as file:
-        data = json.load(file)
+
+    with open(dir_name, 'rb') as binary_data:
+        repaired = fix_mojibake_escapes(binary_data.read())
+    data = json.loads(repaired)
 
     for message in data['messages']:
-        sender = message['sender_name']
-        content = message['content']
-        output_text += f"{sender}:\n{content}\n\n"
+        try:
+            sender = message['sender_name']
+            content = message['content']
+            output_text += f"{sender}:\n{content}\n\n"
+
+        except KeyError:
+            continue
 
 # Save to a text file
 output_filename = "chat_conversation.txt"
